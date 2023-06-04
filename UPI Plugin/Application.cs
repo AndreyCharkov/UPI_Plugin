b1c3595de55f42059f8f1a3a37d8d44f643d635f
Andrey CHHHHH <andreycharkov1@mail.ru>
2023-06-04 19:01:53 +0500

    Добавьте файлы проекта.

---
+++ UPI Plugin/Application.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using Autodesk.Revit.UI;
+using System.Reflection;
+using System.Windows.Media.Imaging;
+using System.IO;
+
+namespace UPI_Plugin
+{
+    public class Application : IExternalApplication
+    {
+        public Result OnShutdown(UIControlledApplication application)
+        {
+            return Result.Succeeded;
+        }
+        public Result OnStartup(UIControlledApplication application)
+        {
+            RibbonPanel panel = RibbonPanel(application);
+            string thisAssemblyPath = Assembly.GetExecutingAssembly().Location;
+
+            if(panel.AddItem(new PushButtonData("Первый плагин","Первый плагин", thisAssemblyPath, "UPI_Plugin.Command")) is PushButton button)
+            {
+                button.ToolTip = "Мой первый плагин";
+
+                Uri uri = new Uri(Path.Combine(Path.GetDirectoryName(thisAssemblyPath), "Resources", "UPI.ico"));
+                BitmapImage bitmap = new BitmapImage(uri);
+                button.LargeImage = bitmap;
+            }
+            return Result.Succeeded;
+        }
+
+        public RibbonPanel RibbonPanel(UIControlledApplication a)
+        {
+            string tabName = "Югорский Проектный Институт";
+            RibbonPanel ribbonPanel = null;
+            //Создание вкладки "Югорский проектный институт"
+            try
+            {
+                a.CreateRibbonTab(tabName);
+            }
+            catch(Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+            }
+            //Создание группы панелей "Архитектура"
+            try
+            {
+                a.CreateRibbonPanel(tabName, "Архитектура");
+                a.CreateRibbonPanel(tabName, "Конструкции");
+                a.CreateRibbonPanel(tabName, "О плагине");
+            }
+            catch(Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+            }
+
+            List<RibbonPanel> panels = a.GetRibbonPanels(tabName);
+            foreach(RibbonPanel p in panels.Where(p => p.Name == "О плагине"))
+            {
+                ribbonPanel = p;
+            }
+
+            return ribbonPanel;
+        }
+    }
+}
---
+++ UPI Plugin/Command.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.Attributes;
+using System.Windows.Forms;
+
+namespace UPI_Plugin
+{
+    [Transaction(TransactionMode.Manual)]
+    public class Command : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            MessageBox.Show("Плагин создан");
+
+            return Result.Succeeded;
+        }
+    }
+}
